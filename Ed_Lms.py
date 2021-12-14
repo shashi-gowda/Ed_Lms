cb6b7b85b5436d21583dd76c0ab6971a94bf22c7
shashi-gowda <91781996+shashi-gowda@users.noreply.github.com>
2021-12-14 18:08:03 +0530

    Update Ed_Lms.py

--- Ed_Lms.py
+++ Ed_Lms.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from datetime import date
 
-
 Students = {}
 Modules = {}
 Units = dict([])
@@ -14,15 +13,15 @@ class manager:
         self.Manager_menu()
         pass
     def Manager_menu(self):
-        print("select your options, Manager! \n 1.Manage Modules \n 2.Manage units \n 3.Manage Students \n 4.Logout")
-        i = int(input("enter your number\n"))
+        print("choose your options from below\n 1.Manage Modules \n 2.Manage units \n 3.Manage Students \n 4.Logout")
+        i = int(input("select your option: "))
         while i not in range(1,4):
             if i == 4:
-                quit()
+                exit()
                 break
             else:
-                print("please enter a valid number\n")
-                i = int(input("please enter your choice , make sure that the number between 1 to 5"))
+                print("please choose a valid number: ")
+                i = int(input("please enter your choice from 1 to 4: "))
         else:
             if i == 1:
                 self.Manage_modules()
@@ -33,12 +32,181 @@ class manager:
             elif i == 3:
                 self.Manage_students()
                 self.Manager_menu()
+                
+    def Manage_modules(self):
+        print("choose from options to:\n 1.create module \n 2.list of modules\n 3.show module\n 4.delete module\n 5.show detailed module\n 6.exit")
+        i = int(input("enter the number\n"))
+        while i not in range(1,6):
+            if i == 6:
+                self.Manager_menu()
+                break
+            else:
+                print("Enter a valid number to choose from below: ")
+                i = int(input("enter the number: "))
+        else:
+            if i == 1:
+                self.create_module()
+                self.Manage_modules()
+            if i == 2:
+                self.view_module()
+                self.Manage_modules()
+            if i == 3:
+                self.show_modules()
+                self.Manage_modules()
+            if i == 4:
+                self.delete_module()
+                self.Manage_modules()
+            if i == 5:
+                self.view_detailed_module()
+                self.Manage_modules()
+    
+    def create_module(self):
+        print("Okay!!, lets create a new module")
+        num = input("Enter a unique key to create new module: ")
+        while num in Modules:
+            print("it seems the module already exist\n")
+            break
+        else:
+            Modules[num] = {}
+            print(" okay seems like module does't exist! please enter the details to create new module")
+            Module_name = input("please enter module name to be created: ")
+            Modules[num]["module name"] = Module_name
+            
+            Modules[num]["id"] = num
+            start_date = input("Enter the starting date of module:  ")
+            Modules[num]["start_date"] = start_date
+            end_date = input("Enter the Ending date: ")
+            Modules[num]["End_date"] = end_date
+            units = input("enter the number of units: ")
+            Modules[num]["units"] = units
+            date = input("please enter today's date: ")
+            Modules[num]["date"] = date
+            i = ["upcoming","ongoing","complete"]
+            if date < start_date:
+                n = i[0]
+                print(i[0])
+            
+            elif date <= end_date and date>=start_date:
+                n = i[1]
+                print(i[1])
+            
+            else:
+                n = i[2]
+                print(i[2])
+            status = n
+            Modules[num]["status"] = status
+            
+    def view_module(self):
+        check_module = input("please enter module name\n")
+        if check_module in Modules:
+            print("the details of the module is: ")
+            for detail in Modules[check_module]:
+                if type(Modules[check_module][detail]) != dict:
+                    print("{},{}".format(detail,Modules[check][detail]))
+                else:
+                    print(detail + ":")
+                    for j in Modules[check][detail]:
+                        print("{},{}".format(detail,Modules[check][detail][j]))
+        else:
+            print("sorry, the module details does not exist\n")
+        print("let's go back to the main menu\n\n")
+        
+    
+    def show_modules(self):
+        module_list = Modules.keys()
+        print("choose modules")
+        temp = input(module_list)
+        if temp not in module_list:
+            print("please choose from available from above module list")
+            return show_module()
+        else:
+            return temp
+        
+    def delete_module(self):
+        num = input("please enter contact number")
+        if num in Modules:
+            del Modules[num]
+        else:
+            print("the module does not exist")
+        print("lets go back to main menu")
+        
+    ## view_detailed module is not there
+    
+    def Manage_units(self):
+        print("select your options! in units \n 1.create unit \n 2. choose units\n 3.update unit\n 4.delete unit\n 5.exit")
+        i = int(input("enter the number\n"))
+        while (i not in range(1,5)):
+            if i == 5:
+                exit()
+                break
+            else:
+                print("enter a valid number, make sure that the number between 1 to 5\n")
+                i = int(input("enter the number: "))
+        else:
+            if i == 1:
+                self.create_unit()
+                self.Manage_units()
+            if i == 2:
+                self.choose_unit()
+                self.Manage_units()
+            if i == 3:
+                self.view_all_units()
+                self.Manage_units()
+            if i == 4:
+                self.delete_unit()
+                self.Manage_units()
+                
+    def create_unit(self):
+        unit_list = list(Modules.keys())
+        print("Okay, lets create a new unit")
+        num = input("please enter unit key does not exist!\n")
+        while num in Units:
+            print("it seems the unit already exist\n")
+            break
+        else:
+            Units[num] = {}
+            print(" okay seems like the module does not exist! please enter the details")
+            unit_name = input("please enter module name!:\n")
+            Units[num]["unit name"] = unit_name
+            unit_type = input("the the type of unit\n")
+            Units[num]["type"] = unit_type
+            
+            Units[num]["id"] = num
+            start_date = input("enter the starting date of module\n ")
+            Units[num]["startdate"] = start_date
+            end_date = input("enter the closing date: \n")
+            
+    def view_all_units():
+        print("the list of units")
+        for i in Units:
+            print(i)
+            
+    def choose_unit():
+        unitlist = (Units.keys())
+        print("choose the unit")
+        tempn = input(unitlist)
+        if tempn not in unitlist:
+            print("please choose freom available units")
+            return choose_unit()
+        else:
+            return tempn
+        
+    def delete_unit(self):
+        num = input("enter the contact number")
+        if num in Units:
+            del Units[num]
+        else:
+            print("the units does not exist")
+        print("lets go back to main menu")
+        
+    
+                
     def Manage_students(self):
-        print("select your options,mange_students menu! \n 1.create student\n 2.view students\n 3.update student\n 4.delete student\n 5.v_students\n 6.quit")
+        print("select your options,mange_students menu! \n 1.create student\n 2.view students\n 3.update student\n 4.delete student\n 5.v_students\n 6.exit")
         i = int(input("enter the number\n"))
         while (i not in range(1,6)):
             if i == 6:
-                quit()
+                exit()
                 break
             else:
                 print("enter a valid number, make sure that the number between 1 to 5\n")
@@ -59,6 +227,7 @@ class manager:
             if i == 5:
                 self.v_students()
                 self.Manage_students()
+                
     def create_student(self):
         print("Okay, lets create a new student")
         num = input("please enter contact number if student does not exist!\n")
@@ -85,6 +254,7 @@ class manager:
             Students[num]["email"] = email
             module = input("enter the course, what he/she want!")
             Students[num]["module"] = module
+            
     def view_student(self):
         check = input("please enter contact number of the student\n")
         if check in Students:
@@ -100,12 +270,7 @@ class manager:
         else:
             print("sorry, the student details does not exist\n")
         print("let's go back to the main menu\n\n")
-    
-    def v_students(self):
-        for i,x in Students.items():
-            print(i,x)
-            
-
+        
     def update_student(self):
         check = input("please enter the contact number of student: ")
         if check in Students:
@@ -133,7 +298,7 @@ class manager:
                             self.delete_student(check)
                             print("student has been sucessfully deleted")
         print("Lets go back to main menu!\n\n")
-
+        
     def delete_student(self):
         num = input("enter the contact number")
         if num in Students:
@@ -141,166 +306,11 @@ class manager:
         else:
             print("the student does not exist")
         print("lets go back to main menu")
-    def Manage_modules(self):
-        print("select your options! in modules \n 1.create module \n 2.list of modules\n 3.show module\n 4.delete module\n 5.show detailed module\n 6.quit")
-        i = int(input("enter the number\n"))
-        while (i not in range(1,6)):
-            if i == 6:
-                quit()
-                break
-            else:
-                print("enter a valid number, make sure that the number between 1 to 3\n")
-                i = int(input("enter the number: "))
-        else:
-            if i == 1:
-                self.create_module()
-                self.Manage_modules()
-            if i == 2:
-                self.view_module()
-                self.Manage_modules()
-            if i == 3:
-                self.show_modules()
-                self.Manage_modules()
-            if i == 4:
-                self.delete_module()
-                self.Manage_modules()
-            if i == 5:
-                self.view_detailed_module()
-                self.Manage_modules()
-    def create_module(self):
-        print("Okay, lets create a new module")
-        num = input("please enter module key does not exist!\n")
-        while num in Modules:
-            print("it seems the module already exist\n")
-            break
-        else:
-            Modules[num] = {}
-            print(" okay seems like the module does not exist! please enter the details")
-            Module_name = input("please enter module name!:\n")
-            Modules[num]["module name"] = Module_name
-            
-            Modules[num]["id"] = num
-            start_date = input("enter the starting date of module ")
-            Modules[num]["startdate"] = start_date
-            end_date = input("enter the closing date: ")
-            Modules[num]["end date"] = end_date
-            units = input("enter the number of units")
-            Modules[num]["units"] = units
-            date = input("please enter today's date")
-            Modules[num]["date"] = date
-            i = ["upcoming","ongoing","complete"]
-            if date < start_date:
-                n = i[0]
-                print(i[0])
-            
-            elif date <= end_date:
-                n = i[1]
-                print(i[1])
-            
-            else:
-                n = i[2]
-                print(i[2])
-            status = n
-            Modules[num]["status"] = status
-    def view_module(self):
-        check = input("please enter module name\n")
-        if check in Modules:
-            print("the details of the module is: ")
-            for detail in Modules[check]:
-                if type(Modules[check][detail]) != dict:
-                    print("{},{}".format(detail,Modules[check][detail]))
-                else:
-                    print(detail + ":")
-                    for j in Modules[check][detail]:
-                        print("{},{}".format(detail,Modules[check][detail][j]))
-        else:
-            print("sorry, the module details does not exist\n")
-        print("let's go back to the main menu\n\n")
         
-    def show_modules(self):
-        module_list = Modules.keys()
-        print("choose modules")
-        temp = input(module_list)
-        if temp not in module_list:
-            print("please choose from available from above module list")
-            return show_module()
-        else:
-            return temp
+    def v_students(self):
+        for i,x in Students.items():
+            print(i,x)
     
-    def delete_module(self):
-        num = input("please enter contact number")
-        if num in Modules:
-            del Modules[num]
-        else:
-            print("the module does not exist")
-        print("lets go back to main menu")
-        
-    def Manage_units(self):
-        print("select your options! in units \n 1.create unit \n 2. choose units\n 3.update unit\n 4.delete unit\n 5.quit")
-        i = int(input("enter the number\n"))
-        while (i not in range(1,5)):
-            if i == 5:
-                quit()
-                break
-            else:
-                print("enter a valid number, make sure that the number between 1 to 5\n")
-                i = int(input("enter the number: "))
-        else:
-            if i == 1:
-                self.create_unit()
-                self.Manage_units()
-            if i == 2:
-                self.choose_unit()
-                self.Manage_units()
-            if i == 3:
-                self.view_all_units()
-                self.Manage_units()
-            if i == 4:
-                self.delete_unit()
-                self.Manage_units()
-    def create_unit(self):
-        unit_list = list(Modules.keys())
-        print("Okay, lets create a new unit")
-        num = input("please enter unit key does not exist!\n")
-        while num in Units:
-            print("it seems the unit already exist\n")
-            break
-        else:
-            Units[num] = {}
-            print(" okay seems like the module does not exist! please enter the details")
-            unit_name = input("please enter module name!:\n")
-            Units[num]["unit name"] = unit_name
-            unit_type = input("the the type of unit\n")
-            Units[num]["type"] = unit_type
-            
-            Units[num]["id"] = num
-            start_date = input("enter the starting date of module\n ")
-            Units[num]["startdate"] = start_date
-            end_date = input("enter the closing date: \n")
-            Units[num]["end date"] = end_date
-            
-    def choose_unit():
-        unitlist = (Units.keys())
-        print("choose the unit")
-        tempn = input(unitlist)
-        if tempn not in unitlist:
-            print("please choose freom available units")
-            return choose_unit()
-        else:
-            return tempn
-    def view_all_units():
-        print("the list of units")
-        for i in Units:
-            print(i)
-    def delete_unit(self):
-        num = input("enter the contact number")
-        if num in Units:
-            del Units[num]
-        else:
-            print("the units does not exist")
-        print("lets go back to main menu")
-        
-        
 class student:
     def __init__(self):
         self.num = input("please enter the contact number: ")
@@ -376,11 +386,6 @@ class student:
         else:
             print("sorry, the module details does not exist\n")
         print("let's go back to the main menu\n\n")
-
-        
-        
-        
-        
         
 def login():
     print("welcome to Switch_Blade's Academy".center(100,"*"))
